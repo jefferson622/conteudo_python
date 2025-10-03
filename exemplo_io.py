675cfcdc9ac306258936778f5e72e52a7244c4b2
Aluno SENAI <Aluno@SN-388038.edu.fiesc.com.br>
2025-10-03 11:20:57 -0300

    aula2: exemplo de entrada do usuario

---
+++ exemplo_io.py
@@ -0,0 +1,10 @@
+nome =input("Digite o seu nome:")
+
+print("Olá,",nome)
+
+quantidade = int(input("Digite a quantidade dep pessoas na sala:"))
+
+print("Há",quantidade,"pessoas na sala",type(quantidade))
+outra_turma = 20
+soma = quantidade + outra_turma
+print("A soma é",soma)
