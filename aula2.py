e9433fc78eb548515285dccf58a452b8e3c5bd52
Aluno SENAI <Aluno@SN-388038.edu.fiesc.com.br>
2025-10-03 11:18:45 -0300

    aula 2: variaveis e tipos

---
+++ aula2.py
@@ -0,0 +1,26 @@
+#Trabalhando com variáveis
+
+#Definindo uma vaiavel com o meu nome
+nome = "jefferson"
+print("O nome é",nome)
+
+palavra = "xulofimpola"
+letra = "A"
+quantidade = 10
+valor = 10.5
+aberto = False
+
+print("O valor de palavra é",palavra,end="")
+print("( tipo: ",type(palavra),")")
+
+print("O valor de letra é",letra,end="")
+print("( tipo: ",type(letra),")")
+
+print("O valor de quantidade é",quantidade,end="")
+print("( tipo: ",type(quantidade),")")
+
+print("O valor de valor é",valor,end="")
+print("( tipo: ",type(valor),")")
+
+print("O valor de aberto é",aberto,end="")
+print("( tipo: ",type(aberto),")")
